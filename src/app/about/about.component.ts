71dfae188865e5c5e70704dd132644bf96c3545f
KayembaJonathan <kayembejonathan5@gmail.com>
2025-01-22 13:09:37 +0200

    Add more code again

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  showEducation: boolean = true;
+  showProjects: boolean = true;
 
+  projects = [
+    { name:'', description:'' }
+  ]
 }
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  linkedIn = '';
+  emailAdress = '';
+
+  constructor (public router: Router){}
+
+  OnHome() {
+    this.router.navigate([])
+  }
+  OnAbout() {
+    this.router.navigate([])
+  }
+  OnContact(){
+    this.router.navigate([])
+  }
+  OnEmail() {
+    this.router.navigate([])
+  }
 }
