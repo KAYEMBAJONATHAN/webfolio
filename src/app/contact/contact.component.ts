2d8d5b243e0da918a85ac471fc0a41c169db47f0
KayembaJonathan <kayembejonathan5@gmail.com>
2025-01-30 10:04:06 +0200

    Add the contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+ userForm: any;
 
+ constructor(public formBuilder: FormBuilder) {
+
+ }
+ ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      name: [null],
+      email: [null],
+      message: [null]
+    })
+ }
+ onSubmit() {
+    console.log('submitted', this.userForm.value);
+ }
 }
